550085155a33549fac1d56ba66dd72d95719e951
YawOtuo <yotuo2003@gmail.com>
2022-10-16 01:24:36 +0000

    increasing margins on overview nav

--- src/components/Page/overview/overview_nav.jsx
+++ src/components/Page/overview/overview_nav.jsx
@@ -7,14 +7,14 @@ export const OverViewNav = () =>
         <div className='container-fluid '>
             <div className='row align-items-start justify-items-center  '>
                 <div className='col-sm-12 mt-5 text-white view_height_100'>
-                    <NavLink to='/home' className={'text-white'}> <div><h5>Home</h5></div></NavLink>
-                    <NavLink to='/home' className={'text-white'}> <div> <h5>Transactions</h5></div></NavLink>
-                    <NavLink to='/home' className={'text-white'}>  <div><h5>Customers</h5></div></NavLink>
-                    <NavLink to='/home' className={'text-white'}>  <div><h5>Balances</h5></div></NavLink>
-                    <NavLink to='/home' className={'text-white'}>  <div><h5>Store</h5></div></NavLink>
-                    <NavLink to='/home' className={'text-white'}>  <div><h5>Payments</h5></div></NavLink>
-                    <NavLink to='/home' className={'text-white'}>  <div><h5>Subaccounts</h5></div></NavLink>
-                    <NavLink to='/home' className={'text-white'}>  <div><h5>Settings</h5></div></NavLink>
+                    <NavLink to='/home' className='text-white'> <div className='mb-3'><h5>Home</h5></div></NavLink>
+                    <NavLink to='/home' className={'text-white'}> <div  className='mb-3'> <h5>Transactions</h5></div></NavLink>
+                    <NavLink to='/home' className={'text-white'}>  <div className='mb-3'><h5>Customers</h5></div></NavLink>
+                    <NavLink to='/home' className={'text-white'}>  <div className='mb-3'><h5>Balances</h5></div></NavLink>
+                    <NavLink to='/home' className={'text-white'}>  <div className='mb-3'><h5>Store</h5></div></NavLink>
+                    <NavLink to='/home' className={'text-white'}>  <div className='mb-3'><h5>Payments</h5></div></NavLink>
+                    <NavLink to='/home' className={'text-white'}>  <div className='mb-3'><h5>Subaccounts</h5></div></NavLink>
+                    <NavLink to='/home' className={'text-white'}>  <div className='mb-3'><h5>Settings</h5></div></NavLink>
 
 
                 </div>
